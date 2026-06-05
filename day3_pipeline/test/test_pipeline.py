4e5016e158f0a605039a3f3d99621068f9fd3c51
Sarthak <24csu185@ncuindia.edu>
2026-06-05 10:26:48 +0530

    Creating test_pipeline.py

--- day3_pipeline/test/test_pipeline.py
+++ day3_pipeline/test/test_pipeline.py
@@ -1,7 +1,4 @@
 from data_loader import load_data
-from preprocess import preprocess_data
-from train import train_model
-from evaluate import evaluate_model
 
 
 def test_load_data():
@@ -11,13 +8,3 @@ def test_load_data():
     assert hasattr(data, "target")
     assert len(data.data) > 0, "Data should not be empty"
     assert len(data.target) > 0, "Target should not be empty"
-
-
-def test_pipeline():
-    data = [1, 2, 3]
-
-    processed = preprocess_data(data)
-    model = train_model(processed)
-    score = evaluate_model(model, processed)
-
-    assert score is not None
